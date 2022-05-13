ac48f826093d38bbbd10dff4290d3aebf91d46a7
WHenderson <github@whenderson.maily.dev>
2022-05-13 17:40:37 +1000

    change landing page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+</script>
+
+<h1>Who Dat?</h1>
+<h2>Play with a friend to guess which celebrity they have chosen!</h2>
